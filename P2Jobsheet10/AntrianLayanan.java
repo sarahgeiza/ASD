f444c99bc7766ff7c46e386e7b781cf13bbcb4e2
sarahgeiza <sarahgeizask@gmail.com>
2026-05-01 15:11:50 +0700

    Percobaan 2

---
+++ P2Jobsheet10/AntrianLayanan.java
@@ -0,0 +1,75 @@
+package P2Jobsheet10;
+
+public class AntrianLayanan {
+    Mahasiswa24[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+
+    public AntrianLayanan(int max) {
+        this.max = max;
+        this.data = new Mahasiswa24[max];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean IsFull() {
+        return size == max;
+    }
+
+    public void tambahAntrian(Mahasiswa24 mhs) {
+        if (IsFull()) {
+            System.out.println("Antrian penuh, tidak dapat menambah mahasiswa.");
+            return;
+        }
+        rear = (rear + 1) % max;
+        data[rear] = mhs;
+        size++;
+        System.out.println(mhs.nama + " berhasil masuk ke antrian.");
+    }
+
+    public Mahasiswa24 layaniMahasiswa() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+            return null;
+        }
+        Mahasiswa24 mhs = data[front];
+        front = (front + 1) % max;
+        size--;
+        return mhs;
+    }
+
+    public void lihatTerdepan() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.print("Mahasiswa terdepan: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[front].tampilkanData();
+        }
+    }
+
+    public void tampilkanSemua() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+            return;
+        }
+        System.out.println("Daftar Mahasiswa dalam Antrian:");
+        System.out.println("NIM - NAMA - PRODI - KELAS");
+        for (int i = 0; i < size; i++) {
+            int index = (front + i) % max;
+            System.out.print((i + 1) + ". ");
+            data[index].tampilkanData();
+        }
+    }
+
+    public int getJumlahAntrian() {
+        return size;
+    }
+}
